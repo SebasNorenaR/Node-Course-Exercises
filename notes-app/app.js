289f8d9bd7917e74a13085143421d1239102d87b
sebastiandanilonr <sebastiandanilonr.dev@gmail.com>
2021-09-03 00:26:05 -0500

    using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,27 @@
 const getNotes = require('./notes');
+const yargs = require('yargs');
 const chalk = require('chalk');
 
-const command = process.argv[2];
-if(command && command === 'add') {
-    console.log('Adding notes');
-} else {
-    console.log('Unknown command');
-}
+// customize yargs version
+yargs.version('1.1.0');
+
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('adding a new note');
+    }
+});
+
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: () => {
+      console.log('removing the note');  
+    }  
+});
+
+// add, remove, read, list
+console.log(yargs.argv);
